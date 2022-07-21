52e8313cb2eeed0f662d41b7cd8513e25a07e2aa
Alex Rodriguez <alexander.joshua.rodriguez@gmail.com>
2022-07-21 10:31:10 -0500

    update to jdbc exercise

---
+++ migration.sql
@@ -0,0 +1,18 @@
+USE adlister_db;
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id VARCHAR(100) NOT NULL,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    );
